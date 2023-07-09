e7dbe3814c761f2872eefb9819e4e6c769b1ef4a
Ganiyat <ganiyatadebayo29@gmail.com>
2023-07-09 09:01:55 +0100

    Added Revenue Table

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -4,3 +4,9 @@ region VARCHAR(10),
 subregion VARCHAR (10),
 segment VARCHAR (5),
 territory VARCHAR(20) NULL)
+
+CREATE TABLE Revenue (
+Revenue_key INT PRIMARY KEY,
+territory_key INT,
+calender_key DATE,
+revenue DECIMAL (7,2) )
