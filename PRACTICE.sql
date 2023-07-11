7b4f3dbeb922098a47ff0e7c5f03c80da8c9eaa7
HAWHY97 <132830782+HAWHY97@users.noreply.github.com>
2023-07-11 21:51:41 +0100

    Add files via upload
    Revenue Table Inserted

--- PRACTICE.sql
+++ PRACTICE.sql
@@ -4,13 +4,3 @@ region VARCHAR(10),
 subregion VARCHAR (10),
 segment VARCHAR (5),
 territory VARCHAR(20) NULL)
-
-CREATE TABLE Revenue (
-Revenue_key INT PRIMARY KEY,
-territory_key INT,
-calender_key DATE,
-revenue DECIMAL (7,2) )
-
-CREATE TABLE Calendar(
-  calendar_key DATETIME PRIMARY KEY NOT NULL,
-  calendardate DATETIME NOT NULL)
